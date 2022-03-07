7a85c6065c0d8ef9265e9c794768978bc646d5cb
olivierzsk <okosminski453@technikumkreatywne.pl>
2022-03-07 10:10:11 +0100

    liczby pierwsze

---
+++ algorytm wyznaczania liczb pierwszych/algorytm wyznaczania liczb pierwszych.cpp
@@ -0,0 +1,14 @@
+﻿using namespace std;
+#include <iostream>
+
+int main()
+{
+    int ilosc;
+    cout << "ile liczb pierwszych";
+    ilosc >> cin;
+    //.int a;
+        //a = 0;
+        //for  {
+
+       // }
+}
