1d01480cfcce13c6ade199dc7cc6670417665387
olivierzsk <okosminski453@technikumkreatywne.pl>
2022-03-21 10:09:34 +0100

    alogorytm

--- algorytm wyznaczania liczb pierwszych/algorytm wyznaczania liczb pierwszych.cpp
+++ algorytm wyznaczania liczb pierwszych/algorytm wyznaczania liczb pierwszych.cpp
@@ -3,12 +3,27 @@
 
 int main()
 {
-    int ilosc;
+    bool flaga;
+    int n;
+    int p;
+    int lp;
+    int d;
+    lp = 0;
     cout << "ile liczb pierwszych";
-    ilosc >> cin;
-    //.int a;
-        //a = 0;
-        //for  {
-
-       // }
+    cin >> n;
+    p = 2;
+    for (lp = 0; lp < n;) {
+        flaga = false;
+        for (d = 2; d < p; d += 1) {
+            if (p % d == 0) {
+                flaga = true;
+            }
+        }
+        if (flaga == false) {
+            cout << p << ",";
+            lp += 1;
+            p += 1;
+        }
+        else { p += 1; }
+    }
 }
