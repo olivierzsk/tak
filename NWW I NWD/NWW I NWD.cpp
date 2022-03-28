87d847a18d5a6376f6fa9c8e6d59666b984eafad
olivierzsk <okosminski453@technikumkreatywne.pl>
2022-03-28 09:59:39 +0200

    nww i nwd

---
+++ NWW I NWD/NWW I NWD.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int a;
+    cout << "podaj liczbe 1: ";
+    cin >> a;
+    int b;
+    cout << "podaj liczbe 2: ";
+    cin >> b;
+    int iloczyn;
+    int nww;
+    iloczyn = a * b;
+    while (a != b) {
+
+        if (a < b) {
+            b = b - a;
+        }
+
+        else {
+            a = a - b;
+        }
+    }
+    nww = iloczyn / a;
+    cout << "NWD:" << a << "\n";
+    cout << "NWW:" << nww;
+}
+
